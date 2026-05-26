238e42cdd8f05f27b6b09cdc04d2e1d033882537
HemanthRoyal74 <hemanthroyalc@gmail.com>
2026-05-27 01:14:07 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://onrender.com",
+        "https://mern-stack-restaurant-reservation-main-fdnv.onrender.com/",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
